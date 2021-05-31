cc816e69976b33e34f559f542e2458606ccfac0c
lenguyenqd <nguyenquocdaile@gmail.com>
2021-05-31 18:42:40 +0700

    add some features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useMemo } from 'react';
+import axios from 'axios';
+import Table from './components/Table/Table';
 import './App.css';
 
+
 function App() {
+  const [employees, setEmployees] = useState([])
+  const [loadingData, setLoadingData] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      await axios
+        .get('https://60b1b78b62ab150017ae121c.mockapi.io/employees')
+        .then((response) => {
+          console.log(response.data);
+
+          setEmployees(response.data)
+          setLoadingData(false);
+        });
+    }
+    if (loadingData) {
+      getData();
+    }
+  }, [])
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Employess',
+        columns: [
+          {
+            Header: 'Name',
+            accessor: 'name',
+          },
+          {
+            Header: 'Email',
+            accessor: 'email',
+          },
+          {
+            Header: 'Position',
+            accessor: 'Position',
+          },
+        ],
+      }
+    ],
+    []
+  )
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        loadingData ? 
+          (<p>Loading Please wait...</p>) 
+        : 
+          ( <Table columns={columns} data={employees} />)
+      }
     </div>
   );
 }
